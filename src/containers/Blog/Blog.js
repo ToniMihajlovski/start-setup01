6ea6d3bd1e02471907de181a8ef451485530d974
ToniMihajlovski <toni@sopro.io>
2019-11-14 22:32:43 +0100

    Using Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -35,9 +35,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
-                <Route path="/" exact component={Posts} />
-                <Route path='/new-post' component={NewPost}/>
-                <Route path='/:id' exact component={FullPost}/>          
+                <Switch>    
+                    <Route path="/" exact component={Posts} />
+                    <Route path='/new-post' component={NewPost}/>
+                    <Route path='/:id' exact component={FullPost}/>          
+                </Switch>
 
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
