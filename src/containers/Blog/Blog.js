465d1d695a7899933a2a045fcb53189bb1df1588
ToniMihajlovski <toni@sopro.io>
2019-11-14 14:56:35 +0100

    Loading Components in Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -22,7 +22,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
+                <Route path="/" exact component={Posts} />
            
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
